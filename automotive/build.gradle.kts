5661f51fa70a70f8acf2ed6c9f3280878e3651b8
Filip Šmíd <filip.smid@eman.cz>
2024-10-25 01:17:02 +0200

    chore: add AndroidX Navigation dependency

--- automotive/build.gradle.kts
+++ automotive/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.navigation.compose)
     implementation(libs.kotlinx.serialization.json)
     implementation(platform(libs.retrofit.bom))
     implementation(libs.retrofit.core)
