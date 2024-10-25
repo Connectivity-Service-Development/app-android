c726a1c407d929a97f6e278d7d4a55ba69b1172e
Filip Šmíd <filip.smid@eman.cz>
2024-10-25 08:45:50 +0200

    feat: define App navigation

--- automotive/src/main/kotlin/cz/eman/skoda/csd/automotive/MainActivity.kt
+++ automotive/src/main/kotlin/cz/eman/skoda/csd/automotive/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : ComponentActivity() {
             ConnectivityServicesDevelopmentTheme(
                 dynamicColor = false,
             ) {
-                // TODO
+                App()
             }
         }
     }
---
+++ automotive/src/main/kotlin/cz/eman/skoda/csd/automotive/MainContainer.kt
@@ -0,0 +1,32 @@
+package cz.eman.skoda.csd.automotive
+
+import android.app.Activity
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import cz.eman.skoda.csd.automotive.feature.dashboard.presentation.view.DashboardScreen
+
+@Composable
+fun App(
+    modifier: Modifier = Modifier,
+) {
+    val navController = rememberNavController()
+    val activity = LocalContext.current as Activity
+    NavHost(
+        modifier = modifier,
+        navController = navController,
+        startDestination = "/",
+    ) {
+        composable(route = "/") {
+            DashboardScreen(
+                onBackClick = activity::onBackPressed,
+                onDiscoverClick = {
+                    navController.navigate("services")
+                },
+            )
+        }
+    }
+}
